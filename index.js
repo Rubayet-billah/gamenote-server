1254be7f7fc510f8096ac9f5387f38c62f3890f6
Rubayet-billah <rubayetbillah909@gmail.com>
2022-11-08 01:17:23 +0600

    get data from mongo

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 // import section
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
+
 
 const port = process.env.PORT || 5000;
 const app = express();
 
+
 // middlewares
 app.use(cors())
 app.use(express.json())
+
+// mongodb operations
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bhwsqpg.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const gameCollection = client.db('gameNote').collection('games')
+
+async function run() {
+    try {
+        app.get('/games', async (req, res) => {
+            const size = parseInt(req.query.size);
+            const games = await gameCollection.find({}).limit(size).toArray();
+            res.send(games)
+        })
+    }
+    catch (error) {
+
+    }
+}
+run().catch(err => console.error(err))
 
 
 
